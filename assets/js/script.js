87b34bcd2b859f4ac257178ff46f05eb373e0926
FernandoGT3 <luiz.moloni@gmail.com>
2023-10-08 17:01:45 -0300

    Funções JS de Dia e Hora

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,34 @@
-() =>{
-    console.log('Hello World');
+function updateClock (){
+    const now = new Date();
+
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const seconds = now.getSeconds().toString().padStart(2, '0');
+
+    const timeString = `Horas: ${hours}:${minutes}:${seconds}`;
+
+    document.querySelector('#clock').innerText = timeString;
 }
+
+function updateDate (){
+    const now = new Date();
+
+    const year = now.getFullYear();
+    const month = now.getMonth();
+    const day = now.getDate();
+
+    const dateString = `Data: ${day}/${month}/${year}`;
+
+    document.querySelector('#date').innerText = dateString;
+}
+
+function fetchAnalogClock (){
+
+}
+
+setInterval(updateClock, 1000);
+setInterval(fetchAnalogClock, 1000);
+
+updateClock();
+updateDate();
+fetchAnalogClock();
