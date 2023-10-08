bdde5fdcdc03145ba8b492faed8d0a7e2a90f082
FernandoGT3 <luiz.moloni@gmail.com>
2023-10-08 18:07:42 -0300

    Relógio Analógico Adicionado

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,16 +14,92 @@ function updateDate (){
     const now = new Date();
 
     const year = now.getFullYear();
-    const month = now.getMonth();
+    let month = now.getMonth();
     const day = now.getDate();
+    let dayOfWeek = now.getDay();
 
-    const dateString = `Data: ${day}/${month}/${year}`;
+    switch (dayOfWeek) {
+        case 0:
+            dayOfWeek = 'Domingo';
+            break;
+        case 1:
+            dayOfWeek = 'Segunda-feira';
+            break;
+        case 2:
+            dayOfWeek = 'Terça-feira';
+            break;
+        case 3:
+            dayOfWeek = 'Quarta-feira';
+            break;
+        case 4:
+            dayOfWeek = 'Quinta-feira';
+            break;
+        case 5:
+            dayOfWeek = 'Sexta-feira';
+            break;
+        case 6:
+            dayOfWeek = 'Sábado';
+            break;
+    }
+
+    switch (month) {
+        case 0:
+            month = 'Janeiro';
+            break;
+        case 1:
+            month = 'Fevereiro';
+            break;
+        case 2:
+            month = 'Março';
+            break;
+        case 3:
+            month = 'Abril';
+            break;
+        case 4:
+            month = 'Maio';
+            break;
+        case 5:
+            month = 'Junho';
+            break;
+        case 6:
+            month = 'Julho';
+            break;
+        case 7:
+            month = 'Agosto';
+            break;
+        case 8:
+            month = 'Setembro';
+            break;
+        case 9:
+            month = 'Outubro';
+            break;
+        case 10:
+            month = 'Novembro';
+            break;
+        case 11:
+            month = 'Dezembro';
+            break;
+    }
+
+    const dateString = `${dayOfWeek}, ${day} de ${month} de ${year}`;
 
     document.querySelector('#date').innerText = dateString;
 }
 
 function fetchAnalogClock (){
+    const now = new Date();
 
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    const seconds = now.getSeconds();
+
+    const hoursDegrees = ((hours / 12) * 360) + 90;
+    const minutesDegrees = ((minutes / 60) * 360) + 90;
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+
+    document.querySelector('#hours-hand').style.transform = `rotate(${hoursDegrees}deg)`;
+    document.querySelector('#minutes-hand').style.transform = `rotate(${minutesDegrees}deg)`;
+    document.querySelector('#seconds-hand').style.transform = `rotate(${secondsDegrees}deg)`;
 }
 
 setInterval(updateClock, 1000);
